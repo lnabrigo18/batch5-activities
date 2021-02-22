28325cc8c9d86beae71eaedf0b26e6dd8093bb5a
Louvien Abrigo <lnabrigo@gmail.com>
2021-02-22 19:39:18 +0800

    app.js

--- Caloriemeter-API/app.js
+++ Caloriemeter-API/app.js
@@ -9,15 +9,44 @@
       });
 
 // Calculator Code
+document.querySelector("#submit-calculate").addEventListener("click",function(){
+  let nameP = document.getElementById("inputName").value;
+  let age = document.getElementById("inputAge").value;
+  var obj = document.getElementById("inputGender");
+  let gender= obj.options[obj.selectedIndex].text;
+  let weight = document.getElementById("inputHeight").value;
+  let height = document.getElementById("inputWeight").value;  
 
+ var result=0;
+    if (gender == 'Female' ){
+      result = 447.593 + (9.247 * weight) + (3.098 * height) - (4.33 * age);
+          }  
+          else {
+           result = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+          }
 
-// Calculator End Code
+  document.getElementById("disName").innerHTML= nameP;
+  document.getElementById("disMaintain").innerHTML= Math.round(result);
+  document.getElementById("disLoss").innerHTML= Math.round(result * 0.77);
+  document.getElementById("disGain").innerHTML= Math.round(result * 1.23);
+  document.getElementById("inputName").value = "";
+  document.getElementById("inputAge").value = "";
+  document.getElementById("inputGender").value = "Gender";
+  document.getElementById("inputHeight").value = "";
+  document.getElementById("inputWeight").value = "";  
+
+})
+
 
 // Counter Code
-var main = document.querySelector('.name');
-var calories= document.querySelector('.calories');
-document.querySelector("#submit").addEventListener("click",function(){
-let inputValue = document.getElementById("inputValue").value;
+  var x = 0;
+  var array = Array();
+
+function newElement()
+{
+
+  var inputValue = document.getElementById("text1").value;
+  
 fetch(`https://calorieninjas.p.rapidapi.com/v1/nutrition?query=${inputValue}`, {
 	"method": "GET",
 	"headers": {
@@ -26,12 +55,45 @@ fetch(`https://calorieninjas.p.rapidapi.com/v1/nutrition?query=${inputValue}`, {
   }
 })
 .then(response => response.json())
-.then(data => { //console.log(data);
-	main.innerHTML = "Name:" + data.items[0].name + " " + "Calories:" + data.items[0].calories + "cal";
-})
+.then(data => {
+
+array[x] = Math.round(data.items[0].calories);
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    var li = document.createElement("li");
+    document.getElementById("myUL").appendChild(li);
+  }
+  li.innerHTML = inputValue + " - " + array[x];x++;
+
+  var e = "";   
+    
+   for (var y=0; y<array.length; y++)
+   {
+     e += array[y] + "+" + "0";
+   }
+   
+   console.log(e);
+   let total = eval(e);
+   
+   document.getElementById("sumCal").innerHTML = 'Total Calories: ' + Math.round(total) + ' cal';
+   document.getElementById("text1").value = "";
+   
+}
+)
 .catch(err => {
 	console.error(err);
-});
+}); }
 
-})
-// Counter Code End
+function clearData() {
+  document.getElementById("disName").innerHTML = "";
+  document.getElementById("disMaintain").innerHTML = "";
+  document.getElementById("disLoss").innerHTML = "";
+  document.getElementById("disGain").innerHTML = "";
+}
+
+function clearData2() {
+  document.getElementById("myUL").innerHTML = "";
+  document.getElementById("sumCal").innerHTML = "";
+}
