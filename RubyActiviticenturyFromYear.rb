1face0393ba2f664b7c449eec1c769388cb76187
lnabrigo18 <73531244+lnabrigo18@users.noreply.github.com>
2021-03-16 19:31:05 +0800

    Create RubyActiviticenturyFromYear.rb

---
+++ RubyActiviticenturyFromYear.rb
@@ -0,0 +1,13 @@
+def centuryFromYear(year)
+  
+   if (year % 100) == 0
+     year/100
+   else 
+     (year - (year % 100))/100 + 1
+   end
+end
+
+puts centuryFromYear(1705)
+puts centuryFromYear(1900)
+puts centuryFromYear(1601)
+puts centuryFromYear(2000)
